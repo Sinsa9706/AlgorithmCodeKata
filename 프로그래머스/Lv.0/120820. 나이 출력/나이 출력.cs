cc50cc0be479e746591088fcb4d77a6af659a6d0
Sinsa9706 <154485256+Sinsa9706@users.noreply.github.com>
2024-01-16 09:04:09 +0900

    [level Lv.0] Title: 나이 출력, Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 2022 - age + 1;
+        
+        if(age > 0 && age <= 120);
+        
+        return answer;
+    }
+}
