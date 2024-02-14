c2eb989546c9edf74527eb192ecf7192b0a490cf
Sinsa9706 <154485256+Sinsa9706@users.noreply.github.com>
2024-02-14 10:02:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solution(int num) {
+        long temp = num;
+        int answer = 0;
+        
+        while(temp != 1){
+            ++answer;
+            
+            temp = temp % 2 == 0 ? temp / 2 : temp * 3 + 1;
+            
+            if(answer > 500){
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
